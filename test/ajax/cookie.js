1127a9f20661de3ecddd11ef4b117658bef05b96
jinpu <jinpu@s254.xrea.com>
2014-01-19 01:00:53 +0900

    cookie.js追加

---
+++ test/ajax/cookie.js
@@ -0,0 +1,40 @@
+var Cookie = (function () {
+	var Cookie = function () {
+	};
+
+	(function () {
+		var cookies = document.cookie;
+		
+		cookies = cookies.split(";");
+		
+		Cookie.values = {};
+		
+		for(var i=0, len = cookies.length ; i < len; i++)
+		{
+			var pair = cookies[i].replace(/^ +| +$/, "").split("=");
+			
+			Cookie.values[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+		}
+	})();
+	
+	Cookie.get = function (name) {
+		if(typeof Cookie.values[name] === "undefined") return "";
+		else return Cookie.values[name];
+	};
+	
+	Cookie.set = function (name, value, expires) {
+		if(typeof expires === "undefined") expires = 0;
+		if(!(expires instanceof Date)) 
+		{
+			expires = (function (sec) {
+				expires = new Date();
+				expires.setTime(sec);
+				return expires;
+			})(expires);
+		}
+		
+		document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; " + "expires=" + expire.toUTCString();
+	}
+	
+	return Cookie;
+})();
